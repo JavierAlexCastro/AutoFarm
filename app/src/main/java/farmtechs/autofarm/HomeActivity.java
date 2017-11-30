1777946e06ce6b859e550d7e0fc1211d6f47f14c
JavierAlexCastro <8484+JavierAlexCastro@users.noreply.github.com>
2017-11-30 10:32:55 -0600

    Changed water menu to be a custom dialog because it's too simple to be an activity of its own. Also adding images, that were not added with the initial commit.

--- app/src/main/java/farmtechs/autofarm/HomeActivity.java
+++ app/src/main/java/farmtechs/autofarm/HomeActivity.java
@@ -1,14 +1,21 @@
 package farmtechs.autofarm;
 
 import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity {
+
+    int pressed1 = 0;
+    int pressed2 = 0;
+    int pressed3 = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,8 +38,87 @@ public class HomeActivity extends AppCompatActivity {
         water_btn.setOnClickListener(new View.OnClickListener()
         {
             public void onClick(View v) {
-                Intent water = new Intent(HomeActivity.this, WaterActivity.class);
-                startActivity(water);
+
+                final Dialog dialog = new Dialog(HomeActivity.this);
+                dialog.setContentView(R.layout.activity_water);
+
+                final Button block_one = (Button) dialog.findViewById(R.id.block1_image);
+                final Button block_two = (Button) dialog.findViewById(R.id.block2_image);
+                final Button block_three = (Button) dialog.findViewById(R.id.block3_image);
+
+                block_one.setOnClickListener(new View.OnClickListener()
+                {
+                    public void onClick(View v) {
+                        if(pressed1==0) {
+                            block_one.setBackgroundResource(R.drawable.green_square);
+                            pressed1=1;
+                        }
+                        else{
+                            block_one.setBackgroundResource(R.drawable.black_square);
+                            pressed1=0;
+                        }
+                    }
+                });
+
+                block_two.setOnClickListener(new View.OnClickListener()
+                {
+                    public void onClick(View v) {
+                        if(pressed2==0) {
+                            block_two.setBackgroundResource(R.drawable.green_square);
+                            pressed2=1;
+                        }
+                        else{
+                            block_two.setBackgroundResource(R.drawable.black_square);
+                            pressed2=0;
+                        }
+                    }
+                });
+
+                block_three.setOnClickListener(new View.OnClickListener()
+                {
+                    public void onClick(View v) {
+                        if(pressed3==0) {
+                            block_three.setBackgroundResource(R.drawable.green_square);
+                            pressed3=1;
+                        }
+                        else{
+                            block_three.setBackgroundResource(R.drawable.black_square);
+                            pressed3=0;
+                        }
+                    }
+                });
+
+                Button water = (Button) dialog.findViewById(R.id.water_btn);
+                // if button is clicked, close the custom dialog
+                water.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+
+                        if (pressed1 == 0 && pressed2 == 0 && pressed3 == 0) {
+                            Toast.makeText(HomeActivity.this, "Select at least one block",
+                                    Toast.LENGTH_SHORT).show();
+                        } else {
+
+                            /**call different functions based on pressed1, pressed2, pressed3 values (Raspberry Pi)**/
+
+                            block_one.setBackgroundResource(R.drawable.black_square);
+                            block_two.setBackgroundResource(R.drawable.black_square);
+                            block_three.setBackgroundResource(R.drawable.black_square);
+                            pressed1 = 0;
+                            pressed2 = 0;
+                            pressed3 = 0;
+
+                            Toast.makeText(HomeActivity.this, "Watering plant(s)",
+                                    Toast.LENGTH_SHORT).show();
+                            dialog.dismiss();
+                        }
+                    }
+                });
+
+                dialog.show();
+                Window window = dialog.getWindow();
+                window.setLayout(1000,1150);
             }
         });
 
@@ -70,8 +156,4 @@ public class HomeActivity extends AppCompatActivity {
                     }
                 }).setNegativeButton("No", null).show();
     }
-
-
-
-
 }
